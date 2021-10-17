eda79de53271c88d28a0f8c8b4528a59a5a0d6b8
Dwayne Sutherland <DwayneaSutherland1@gmail.com>
2021-10-18 00:47:10 +0100

    did mobile nav bar

--- frontend/src/componets/GroupFilter/GroupFilter.js
+++ frontend/src/componets/GroupFilter/GroupFilter.js
@@ -2,20 +2,45 @@ import { useState } from "react";
 
 const GroupFilter = () => {
   const [choice, setChoice] = useState("All");
+  const [dropDownActive, setDropdownActive] = useState(false);
+
+  const ToggleDropdown = () => {
+    setDropdownActive((prev) => !prev);
+  };
+
+  const CloseDropdown = () => {
+    setDropdownActive(false);
+  };
+
+  const SelectOption = () => {
+    CloseDropdown();
+  };
+
   return (
     <div className="Group-Filter">
       <div className="Group-Filter__header">
-        <h4 className="Group-Filter__title">
+        <h4 className="Group-Filter__title" onClick={ToggleDropdown}>
           Sort by: <span className="Group-Filter__choice">{choice}</span>
         </h4>
       </div>
-      <div className="dropdown">
-        <ul className="dropdown__content">
-          <li className="dropdown__item dropdown__item--active">All</li>
-          <li className="dropdown__item">Completed</li>
-          <li className="dropdown__item">Uncompleted</li>
-        </ul>
-      </div>
+      {dropDownActive && (
+        <div className="dropdown" onMouseLeave={CloseDropdown}>
+          <ul className="dropdown__content">
+            <li
+              onClick={SelectOption}
+              className="dropdown__item dropdown__item--active"
+            >
+              All
+            </li>
+            <li onClick={SelectOption} className="dropdown__item">
+              Completed
+            </li>
+            <li onClick={SelectOption} className="dropdown__item">
+              Uncompleted
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
